a1e519ccdfa671b3db9a2e86a0ad99c81483d15a
safirasn21 <safirasn021@gmai.com>
2023-03-24 15:58:22 +0700

    Menambahkan data ke-

--- Insertionsort/Insertionsort.cpp
+++ Insertionsort/Insertionsort.cpp
@@ -22,5 +22,11 @@ void input() {
 	cout << "Masukkan Element Array" << endl;
 	cout << "======================" << endl;
 
+	for (int i = 0; i < n; i++)
+	{
+		cout << "data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
+	}
+
 }
 
