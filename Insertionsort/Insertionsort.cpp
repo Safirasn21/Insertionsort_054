fa4ba1d9ea13343ac263770292cde013050907ff
safirasn21 <safirasn021@gmai.com>
2023-03-24 15:56:55 +0700

    Menambahkan cout untuk menambahkan di layar

--- Insertionsort/Insertionsort.cpp
+++ Insertionsort/Insertionsort.cpp
@@ -17,5 +17,10 @@ void input() {
 			cout << "\nArray yang anda masukkan maksimal 20 elemen.\n";
 		}
 	}
+	cout << endl;
+	cout << "======================" << endl;
+	cout << "Masukkan Element Array" << endl;
+	cout << "======================" << endl;
 
 }
+
